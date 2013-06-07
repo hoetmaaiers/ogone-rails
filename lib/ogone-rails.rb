87d640ff644127087e19d6e05fc9e7f6b6e49c80
robinhoudmeyers <houdmeyers@gmail.com>
2013-06-07 10:51:21 +0200

    use default_config for symbolizing keys

--- lib/ogone-rails.rb
+++ lib/ogone-rails.rb
@@ -11,8 +11,8 @@ require 'ogone-rails/check-auth'
 
 
 module OgoneRails
-	ASSET_ROOT     = File.expand_path((defined?(Rails) && Rails.root.to_s.length > 0) ? Rails.root : ENV['RAILS_ROOT'] || '.') unless defined?(ASSET_ROOT)
-	DEFAULT_CONFIG = File.join( ASSET_ROOT, 'config', 'ogone.yml')
+	APP_ROOT     = File.expand_path((defined?(Rails) && Rails.root.to_s.length > 0) ? Rails.root : ENV['RAILS_ROOT'] || '.') unless defined?(APP_ROOT)
+	DEFAULT_CONFIG = File.join( APP_ROOT, 'config', 'ogone.yml')
 
 	class MissingConfiguration < NameError; end
 
@@ -25,7 +25,7 @@ module OgoneRails
 		raise  MissingConfiguration, "Could not find the #{ config_path } configuration file" unless exists
 
 		# load ogone configuration
-		config = YAML.load_file('config/ogone.yml')[Rails.env].symbolize_keys
+		config = YAML.load_file(DEFAULT_CONFIG)[Rails.env].symbolize_keys
 		OgoneRails::config (config)
 	end
 end
