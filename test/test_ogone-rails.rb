8c778252b3e0d8dff832df465ff18722d0bce1ac
Rafi Adnan <rafiadnan47@gmail.com>
2015-04-08 18:09:51 +0600

    test whether amount is set correctly

--- test/test_ogone-rails.rb
+++ test/test_ogone-rails.rb
@@ -5,4 +5,9 @@ class TestOgoneRails < Test::Unit::TestCase
   should "probably rename this file and start testing for real" do
     flunk "hey buddy, you should probably rename this file and start testing for real"
   end
+
+  should "set amount correctly" do
+    @ogone_fields = OgoneRails::Helpers.ogone_fields(amount: 586.8)
+    assert_includes(@ogone_fields, "<input type='hidden' name='amount' value='58680' />")
+  end
 end
